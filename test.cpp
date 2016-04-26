e2175b83bd5e1f31114b4d624fbbfc47d7e897ba
lugakorrav <lugakorrav@yandex.ru>
2016-04-26 20:12:06 +0300

    0.2

---
+++ test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "skiplist.h"
+#include <set>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+	set<int> s;
+	skiplist<int> sl;
+	int start_set = clock();
+	for (int i = 0; i < 10000; i++)
+	{
+		s.insert(rand() % 10000);
+	}
+	int stop_set = clock();
+	int start_sl = clock();
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++)
+	{
+		sl.insert(*it);
+	}
+	int stop_sl = clock();
+	cout << s.size() << endl;
+	cout << sl.size() << endl;
+
+	set<int>::iterator it1 = s.begin();
+	skiplist<int>::iterator it2 = sl.begin();
+	for (; it1 != s.end(); it1++, it2++)
+	{
+		if (*it1 != *it2)
+		{
+			cout << "something went wrong";
+		}
+	}
+
+	cout << stop_set - start_set << endl;
+	cout << stop_sl - start_sl << endl;
+
+	return 0;
+}
